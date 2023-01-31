76dc822b43366b9ce1051d7817b859717b5d2674
Styles Weiler <19nstylesweiler@gmail.com>
2023-01-30 19:34:49 -0700

    Add project files.

---
+++ Project4/Startup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Project4
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDefaultFiles(); // the landing route "/" will be the index.html page
+            app.UseStaticFiles(); // allows you to use static HTML files rather than dynamically created pages
+;        }
+    }
+}
---
+++ Project4/wwwroot/js/grader.js
@@ -0,0 +1,71 @@
+﻿$("#buttonSubmit").click(function () {
+    // percentages for each assignment category as put in by the user
+    // assigns each value to a new variable
+    let assignments = $("#assignments").val();
+    let quizzes = $("#quizzes").val();
+    let intex = $("#intex").val();
+    let groupprojects = $("#groupprojects").val();
+    let exams = $("#exams").val();
+
+    // Weight of assignment categories
+
+    // Assignments: 53.76%
+    let weightAssignment = 0.50;
+    // Group: 5.38%
+    let weightGroupProject = 0.10;
+    // Quizzes: 8.6%
+    let weightQuizzes = 0.10;
+    // INTEX: 10.75%
+    let weightIntex = 0.10;
+    // Exams: 21.51%
+    let weightExams = 0.10;
+
+    // calculating the total percentage earned in the class
+    let totalPercentage = (assignments * weightAssignment) + (quizzes * weightQuizzes) + (intex * weightIntex) + (groupprojects * weightGroupProject) + (exams * weightExams);
+
+    // outputting the results of the calculation above
+    $("#percentgrade").text(totalPercentage + "%");
+
+    let lettergrade = 'N';
+    // Based on grade percentage, determine a letter grade
+
+    if (totalPercentage >= 94) {
+        lettergrade = 'A';
+    }
+    else if (totalPercentage >= 90) {
+        lettergrade = 'A-';
+    }
+    else if (totalPercentage >= 87) {
+        lettergrade = 'B+';
+    }
+    else if (totalPercentage >= 84) {
+        lettergrade = 'B';
+    }
+    else if (totalPercentage >= 80) {
+        lettergrade = 'B-';
+    }
+    else if (totalPercentage >= 77) {
+        lettergrade = 'C+';
+    }
+    else if (totalPercentage >= 74) {
+        lettergrade = 'C';
+    }
+    else if (totalPercentage >= 70) {
+        lettergrade = 'C-';
+    }
+    else if (totalPercentage >= 67) {
+        lettergrade = 'D+';
+    }
+    else if (totalPercentage >= 64) {
+        lettergrade = 'D';
+    }
+    else if (totalPercentage >= 60) {
+        lettergrade = 'D-';
+    }
+    else if (totalPercentage >= 0) {
+        lettergrade = 'E'
+    }
+
+    // output the letter grade determined in the above if statement block
+    $("#lettergrade").text(lettergrade);
+});
